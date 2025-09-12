f36c443207c3be10c8d60afac5f074df86601b47
Tobias Meier <meier.tobias@hotmail.com>
2025-09-12 15:44:38 +0200

    pipeline and pages updated to build two separated pdf's

--- build_latex_local.sh
+++ build_latex_local.sh
@@ -1,6 +1,5 @@
 #!/bin/sh
-COMMAND="latexmk -xelatex -cd -interaction=nonstopmode -file-line-error -jobname=main_full -outdir=../build ./src/main.tex ; 
-    sed '0,/{1}/s//{0}/' -i src/var.tex ; 
-    latexmk -xelatex -cd -interaction=nonstopmode -file-line-error -jobname=main -outdir=../build ./src/main.tex ;
-    sed '0,/{0}/s//{1}/' -i src/var.tex"
+COMMAND="latexmk -xelatex -cd -interaction=nonstopmode -file-line-error -jobname=main_summary -outdir=../build ./src/main_summary.tex ; 
+    latexmk -xelatex -cd -interaction=nonstopmode -file-line-error -jobname=main_cheatsheet -outdir=../build ./src/main_cheatsheet.tex ;
+    latexmk -xelatex -cd -interaction=nonstopmode -file-line-error -jobname=main -outdir=../build ./src/main.tex"
 exec docker run --rm -i --user="$(id -u):$(id -g)" --net=none --env HOME=../build -v "$PWD":/usr/src/app -w /usr/src/app texlive/texlive:latest sh  -c "$COMMAND"
